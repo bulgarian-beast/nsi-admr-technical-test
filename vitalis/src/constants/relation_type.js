86569b366790a55d420855f3d60c90da8bde1869
Sofian Belahouel <sofian.belahouel@epitech.eu>
2025-03-16 20:04:13 +0100

    ADDING --> ./vitalis/src/constants/relation_type.js {}

---
+++ vitalis/src/constants/relation_type.js
@@ -0,0 +1,12 @@
+/**
+** Author: Sofian Belahouel
+** Filename:
+** File description: This file contains all the functions that allow us to
+** Create Time: 2025-03-16 16:30:18
+** Modified by: Sofian Belahouel
+** Modified time: 2025-03-16 16:31:03
+**/
+
+const ALLOWED_ENTRIES = ["WIFE", "HUSBAND", "MOTHER", "FATHER", "SISTER", "BROTHER", "OTHER"];
+
+exports.ALLOWED_ENTRIES = ALLOWED_ENTRIES;

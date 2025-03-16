d1d6e15601c79852b158e35c5a21ba1c1846587f
Sofian Belahouel <sofian.belahouel@epitech.eu>
2025-03-16 16:55:45 +0100

    UPDATING --> dashboard/api/v1 {Built /register-contact endpoint with xss attack and sql injection mitigation}

---
+++ dashboard/api/v1/constants/regex.js
@@ -0,0 +1,12 @@
+/**
+** Author: Sofian Belahouel
+** Filename:
+** File description: This file contains all the functions that allow us to
+** Create Time: 2025-03-16 16:48:04
+** Modified by: Sofian Belahouel
+** Modified time: 2025-03-16 16:48:06
+**/
+
+const digitOnly = /^\d+$/;
+
+exports.digitOnly = digitOnly;
---
+++ dashboard/api/v1/router.js
@@ -0,0 +1,55 @@
+/**
+** Author: Sofian Belahouel
+** Filename:
+** File description: This file contains all the functions that allow us to
+** Create Time: 2025-03-16 15:28:25
+** Modified by: Sofian Belahouel
+** Modified time: 2025-03-16 15:28:33
+**/
+
+const router = require("express").Router();
+const { check, validationResult } = require('express-validator');
+const relationType = require("./constants/relation_type.js");
+const regex = require("./constants/regex.js");
+
+function handleValidationErrors(req, res, next) {
+    console.log(req);
+    const errors = validationResult(req);
+
+    console.log("Errors:");
+    console.log(errors);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            status: "error",
+            message: "Validation failed",
+            errors: errors.array().map(err => ({
+                field: err.param,
+                code: err.msg.code,
+                message: err.msg.message
+            })),
+        });
+    }
+    next();
+}
+
+router.post("/register-contact", [
+    check('email').trim().escape().isEmail().normalizeEmail()
+        .withMessage({ code: "ERROR_REGISTER_EMAIL", message: "Invalid email format" }),
+    check("patientID").trim().escape().matches(regex.digitOnly)
+        .withMessage({code: "ERROR_REGISTER_PATIENT_ID", message: "Patient ID must contain only digits"}),
+    check("lastName").trim().escape().isLength({ min: 3, max: 255 })
+        .withMessage({ code: "ERROR_REGISTER_LAST_NAME", message: "Last name must be between 3 and 255 characters" }),
+    check("firstName").trim().escape().isLength({ min: 3, max: 255 })
+        .withMessage({ code: "ERROR_REGISTER_FIRST_NAME", message: "First name must be between 3 and 255 characters" }),
+    check("relation").trim().escape().isIn(relationType.ALLOWED_ENTRIES)
+        .withMessage({ code: "ERROR_REGISTER_RELATION", message: `Relation must be one of: ${relationType.ALLOWED_ENTRIES.join(", ")}` }),
+    handleValidationErrors
+], (req, res) => {
+    res.status(200).json({});
+
+    // TO DO: Send new rabbit mq entry;
+    
+
+});
+
+module.exports = router;

e41a0e18723467592adbea299a8197234d790457
Sofian Belahouel <sofian.belahouel@epitech.eu>
2025-03-16 20:07:31 +0100

    UPDATING --> dashboard/src/api/v1/router.js {Added action and data parameters to msg payload}

--- dashboard/src/api/v1/router.js
+++ dashboard/src/api/v1/router.js
@@ -33,7 +33,7 @@ router.post("/register-contact", [
     check('email').trim().escape().isEmail().normalizeEmail()
         .withMessage({ code: "ERROR_REGISTER_EMAIL", message: "Invalid email format" }),
     check("patientID").trim().escape().matches(regex.digitOnly)
-        .withMessage({code: "ERROR_REGISTER_PATIENT_ID", message: "Patient ID must contain only digits"}),
+        .withMessage({ code: "ERROR_REGISTER_PATIENT_ID", message: "Patient ID must contain only digits" }),
     check("lastName").trim().escape().isLength({ min: 3, max: 255 })
         .withMessage({ code: "ERROR_REGISTER_LAST_NAME", message: "Last name must be between 3 and 255 characters" }),
     check("firstName").trim().escape().isLength({ min: 3, max: 255 })
@@ -43,12 +43,15 @@ router.post("/register-contact", [
     handleValidationErrors
 ], async (req, res) => {
     const message = {
-        patientID: req.body.patientID,
-        email: req.body.email,
-        lastName: req.body.lastName,
-        firstName: req.body.firstName,
-        relation: req.body.relation,
-        timestamp: new Date().toISOString()
+        action: "INSERT_ONE",
+        data: {
+            patientID: req.body.patientID,
+            email: req.body.email,
+            lastName: req.body.lastName,
+            firstName: req.body.firstName,
+            relation: req.body.relation,
+        },
+        timestamp: new Date().toISOString(),
     };
 
     try {
